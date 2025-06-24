255074c676d2fa7ad3860a3f70e9cd1b58dee088
Dima <dimaff402@gmail.com>
2025-06-24 19:21:29 +0600

    Add files via upload

---
+++ dell/HomeWork4_4/apps/main/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from apps.main.models import Setting, Gallary, Form
+# Register your models here.
+
+# admin.site.register(Setting)
+
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'logo')
+    list_editable = ("logo",)
+    list_filter = ("title", "description")
+    search_fields = ("title",)
+
+@admin.register(Gallary)
+class GallaryAdmin(admin.ModelAdmin):
+    list_display = ('image',)
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('name','message')
